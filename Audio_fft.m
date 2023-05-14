f5f49ca51b625e6b96ba284c0a507f931ac38af9
MagnusSejerLind <magnussejerlind@gmail.com>
2023-05-14 13:59:07 +0200

    ignored

--- Audio_fft.m
+++ Audio_fft.m
@@ -1,20 +1,38 @@
 clc,clear,close all
 
 % Import data
-[data,fs] = audioread("C:\Dokumenter_ny\Bachelor project\Test audio recordings\wind_tunnel_inmic.wav");
-
+[data,fs] = audioread("C:\Dokumenter_ny\Bachelor project\Test audio recordings\cylinder.wav");
 
 
 % Variables of data
-N = length(data);       % no. samples
+N = length(data);       % Number of samples
 dt = 1/fs;              % Sample time
 T = length(data)/fs;    % Total time
 f_max = fs/2;           % Maximum frequency
 f_min = 1/T;            % Minimum frequency
+t = 0:5:T;              % Time vector
 
 % Domain
 fspan = linspace(f_min,f_max,N/2);
 tspan = 0:dt:T-dt;
+
+figure()
+set(0,'defaultTextInterpreter','latex');
+plot(tspan,data);
+
+
+% xticks(0:5:30)
+% set(gca,'XTick',0:5:30,7)
+% aud.MarkerIndices = 0:5:30
+title('Audio recording')
+ylabel('Amplitude')
+% xlabel('time [s]')
+
+
+
+
+
+
 
 % Fourier transform
 FT = fft(data)'/N;
@@ -29,11 +47,12 @@ YP = FT(1:N/2);
 P = abs(YP).^2;
 
 % Figure
+figure()
 stem(fspan,P,'.','MarkerFaceColor','blue','MarkerSize',15)
-grid
 title('Power Spectrum')
 xlabel('Frequency [Hz]')
 ylabel('Power')
+grid
 
 % Extract dominant frequency
 domf_i = find(P == max(P));
