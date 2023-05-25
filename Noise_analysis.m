eaefc1258ff24758609789cd37e830a8b33867e7
MagnusSejerLind <magnussejerlind@gmail.com>
2023-05-25 18:02:40 +0200

    Update Noise_analysis.m

--- Noise_analysis.m
+++ Noise_analysis.m
@@ -2,19 +2,46 @@
 % Import audio recordings, and calls Audio_vis to visualize the data
 
 clc,clear,close all
+
+
+
+%% Options
+% Set to 1 to activate
+
+option.save = 0;    % Not working 
+
+option.print = 0;
+
+% Include types of tests
+option.untriped = 1;
+option.tripped = 0;
+option.background = 0;
+
+% Plot options
+option.recording = 0;
+option.fft = 0;
+option.loglog = 0;
+option.recording_tiled = 0;
+option.fft_tiled = 0;
+option.loglog_tiled = 0;
+option.SPL = 1;
+
+
 %% Import data
 
 % Reference
 [reference] = audioread('Audio_recordings\Reference_sound.wav');
 
 % Background
+if option.background == 1
 [main.Background_off.data,main.Background_off.fs] = audioread("Audio_recordings\Background\Background_off.wav");
 [main.Background_no_rod.data,main.Background_no_rod.fs] = audioread("Audio_recordings\Background\Background_no_rod.wav");
 [main.Background_rod_1.data,main.Background_rod_1.fs] = audioread("Audio_recordings\Background\Background_rod_1.wav");
 [main.Background_rod_2.data,main.Background_rod_2.fs] = audioread("Audio_recordings\Background\Background_rod_2.wav");
-
+end
 
 % Untripped flow
+if option.untriped == 1
 [main.UT_0012_s_a0_01.data,main.UT_0012_s_a0_01.fs] = audioread("Audio_recordings\Untripped\UT_0012_s_a0_01.wav");
 [main.UT_0012_b_a0_01.data,main.UT_0012_b_a0_01.fs] = audioread("Audio_recordings\Untripped\UT_0012_b_a0_01.wav");
 [main.UT_0012_s_a10_01.data,main.UT_0012_s_a10_01.fs] = audioread("Audio_recordings\Untripped\UT_0012_s_a10_01.wav");
@@ -34,9 +61,10 @@ clc,clear,close all
 [main.UT_64421_b_a0_01.data,main.UT_64421_b_a0_01.fs] = audioread("Audio_recordings\Untripped\UT_64421_b_a0_01.wav");
 [main.UT_64421_s_a10_01.data,main.UT_64421_s_a10_01.fs] = audioread("Audio_recordings\Untripped\UT_64421_s_a10_01.wav");
 [main.UT_64421_b_a10_01.data,main.UT_64421_b_a10_01.fs] = audioread("Audio_recordings\Untripped\UT_64421_b_a10_01.wav");
-
+end
 
 % Tripped flow
+if option.tripped == 1
 [main.TF_0012_s_a0_01.data,main.TF_0012_s_a0_01.fs] = audioread("Audio_recordings\Tripped\TF_0012_s_a0.wav");
 [main.TF_0012_b_a0_01.data,main.TF_0012_b_a0_01.fs] = audioread("Audio_recordings\Tripped\TF_0012_b_a0.wav");
 [main.TF_0012_s_a10_01.data,main.TF_0012_s_a10_01.fs] = audioread("Audio_recordings\Tripped\TF_0012_s_a10.wav");
@@ -56,28 +84,15 @@ clc,clear,close all
 [main.TF_64421_b_a0_01.data,main.TF_64421_b_a0_01.fs] = audioread("Audio_recordings\Tripped\TF_64421_b_a0.wav");
 [main.TF_64421_s_a10_01.data,main.TF_64421_s_a10_01.fs] = audioread("Audio_recordings\Tripped\TF_64421_s_a10.wav");
 [main.TF_64421_b_a10_01.data,main.TF_64421_b_a10_01.fs] = audioread("Audio_recordings\Tripped\TF_64421_b_a10.wav");
+end
 
 
-%% Options
-% Set to 1 to activate
-
-option.save = 0;    % Not working 
-
-option.print = 0;
-
-% Plot options
-option.recording = 0;
-option.fft = 0;
-option.loglog = 0;
-option.recording_tiled = 0;
-option.fft_tiled = 0;
-option.loglog_tiled = 0;
 
 
 
 %% Set data scale
 
-T = 10; % Set length of recording to by analyzed [s]
+T = 10; % Set new length of recording to by analyzed [s]
 
 fn_main = fieldnames(main);
 [corrected_audio] = correct_data(main,T);
@@ -104,9 +119,12 @@ Audio_vis_tiled(results,main,option)
 %% SPL
 % Determine soundpressure level of recordings
 
-for i = 1:numel(fn_main)
-    
+for i = 1:numel(fn_main)    
     results.(fn_main{i}).SPL = Audio_SPL(main.(fn_main{i}).data,reference);
+end
 
 
+% Plot SPL
+if option.SPL == 1
+    SPL_plot(results,fn_main)
 end
