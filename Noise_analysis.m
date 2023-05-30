042fd00970efd4e77cd3b3f233c543980fdfbafe
delrullo <emil.bedsted.andersen@gmail.com>
2023-05-30 17:17:12 +0200

    graphmaker put into function

--- Noise_analysis.m
+++ Noise_analysis.m
@@ -26,7 +26,7 @@ option.recording_tiled = 0;
 option.fft_tiled = 0;
 option.loglog_tiled = 0;
 option.SPL = 0;
-option.graphmaker3000 = 1;
+option.gm3k = 1;
 
 
 option.filter = 1;
@@ -137,27 +137,42 @@ end
 Audio_vis_tiled(results,main,option)
 
 
-%% Audio recording vs Filtered recording
-
+%% Bandstop Filter
+    % Removing fan frequencies
 if option.filter == 1
-N = fs * 10;    % Samples
+N = fs * T;    %  # of Samples
 f1 = 30;        % Lower cutoff frequency (in Hz)
 f2 = 70;        % Upper cutoff frequency (in Hz)
 f3 = 260;       % Lower cutoff frequency (in Hz)
 f4 = 310;       % Upper cutoff frequency (in Hz)
 f5 = 560;       % Lower cutoff frequency (in Hz)
 f6 = 610;       % Upper cutoff frequency (in Hz)
+f7 = 80;
+f8 = 100;
 
 for i = 1:length(fn_main)
     y = bandstop(corrected_audio.(fn_main{i}).data,[f1 f2],fs);
     y = bandstop(y,[f3 f4],fs);
+    y = bandstop(y,[f7 f8],fs);
     corrected_audio.(fn_main{i}).filter = bandstop(y,[f5 f6],fs);
 end
 
+%% Filter for ZOOM plots
+    % Removing fan frequency from zoom plots
+f1 = 47;
+f2 = 49;
+
+for i = 1:length(fn_main)
+    corrected_audio.(fn_main{i}).filter_ZOOM = bandstop(corrected_audio.(fn_main{i}).data,[f1 f2],fs);
+end
+
+
+
+%%
 if option.recording_tiled == 1
     figure()
     t2_rec = tiledlayout('flow');
-    title(t2_rec,'Audio recording vs Filtered audio recording')
+    title(t2_rec,'Audio recording vs Filtered audio recording');
     for i = 1:length(fn_main)
         nexttile
         hold on
@@ -170,19 +185,23 @@ if option.recording_tiled == 1
     end
 end
 
-%% Power vs Filtered Power
+%% FFT of Filtered data
 
 for i = 1:length(fn_main)
     FT = fft(corrected_audio.(fn_main{i}).filter)'/N;
-
+    FT_ZOOM = fft(corrected_audio.(fn_main{i}).filter_ZOOM)'/N;
+        
         % Remove mean value
     FT(1) = [];
+    FT_ZOOM(1) = [];
 
         % Extract detectable frequencies
     YP = FT(1:floor(N/2));
+    YP_ZOOM = FT_ZOOM(1:floor(N/2));
 
         % Determine power
     results.(fn_main{i}).filtered_P = abs(YP).^2;
+    results.(fn_main{i}).filter_ZOOM = abs(YP_ZOOM).^2;
 end
 
 if option.fft_tiled == 1
@@ -216,122 +235,46 @@ if option.loglog_tiled == 1
     end
 end
 
+%% Normalization scheme
+
+ % Method
+% range = max(data)-min(data);
+% normalized(i) = (data(i) - min(data)/range);
+
+% range of all data
+max_d = 0;
+min_d = 10;
+for i = 1:length(fn_main)
+    max_r = max(results.(fn_main{i}).P);
+    min_r = min(results.(fn_main{i}).P);
+    if max_r > max_d
+        max_d = max_r;
+    end
+    if min_r < min_d
+        min_d = min_r;
+    end
+end
+
+range = max_d - min_d;
+
+%% Norm for P
+for i = 1:length(fn_main)
+    for k = 1:length(results.(fn_main{i}).P)
+        results.(fn_main{i}).normalized_P(k) = (results.(fn_main{i}).P(k) - min_d)/range;
+    end
+end
+
+%% Norm for filter_ZOOM
+for i = 1:length(fn_main)
+    for k = 1:length(results.(fn_main{i}).filter_ZOOM)
+        results.(fn_main{i}).normalized_filter_ZOOM(k) = (results.(fn_main{i}).filter_ZOOM(k) - min_d)/range;
+    end
+end
+
+
 %% GraphMaker3000
-
-if option.graphmaker3000 == 1
-set(0,'defaultTextInterpreter','latex');
-
-    % Audio plots
-figure()
-graphmaker3000.N0012 = tiledlayout(3,2);
-title(graphmaker3000.N0012,'NACA 0012')
-for i = 5:10
-    nexttile
-    hold on
-    plot(results.Background_off.tspan,corrected_audio.(fn_main{i}).data)
-    plot(results.Background_off.tspan,corrected_audio.(fn_main {i}).filter)
-    xlabel('$Time \ [s]$')
-    legend({'Audio recording','Filtered audio recording'})
-    title('$Standard \ \alpha0$')
-    hold off
-end
-
-figure()
-graphmaker3000.N5512 = tiledlayout(3,2);
-title(graphmaker3000.N5512,'NACA 5512')
-for i = 11:16
-    nexttile
-    hold on
-    plot(results.Background_off.tspan,corrected_audio.(fn_main{i}).data)
-    plot(results.Background_off.tspan,corrected_audio.(fn_main {i}).filter)
-    xlabel('$Time \ [s]$')
-    legend({'Audio recording','Filtered audio recording'})
-   
-    hold off
-end
-
-figure()
-graphmaker3000.N63418 = tiledlayout(3,2);
-title(graphmaker3000.N63418,'NACA 63-418')
-for i = 17:22
-    nexttile
-    hold on
-    plot(results.Background_off.tspan,corrected_audio.(fn_main{i}).data)
-    plot(results.Background_off.tspan,corrected_audio.(fn_main {i}).filter)
-    xlabel('$Time \ [s]$')
-    legend({'Audio recording','Filtered audio recording'})
-    
-    hold off
-end
-
-figure()
-graphmaker3000.N64421 = tiledlayout(3,2);
-title(graphmaker3000.N64421,'NACA 64-421')
-for i = 23:28
-    nexttile
-    hold on
-    plot(results.Background_off.tspan,corrected_audio.(fn_main{i}).data)
-    plot(results.Background_off.tspan,corrected_audio.(fn_main {i}).filter)
-    xlabel('$Time \ [s]$')
-    legend({'Audio recording','Filtered audio recording'})
-    
-    hold off
-end
-
-    % Power plots
-
-figure()
-graphmaker3000.N0012 = tiledlayout(3,2);
-title(graphmaker3000.N0012,'NACA 0012')
-for i = 5:10
-    nexttile
-    hold on
-    plot(results.Background_off.fspan,results.(fn_main{i}).P)
-    plot(results.Background_off.fspan,results.(fn_main{i}).filtered_P)
-    xlabel('Frequency [Hz]')
-    legend({'Power spectrum','Filtered power spectrum'})
-    hold off
-end
-
-figure()
-graphmaker3000.N5512 = tiledlayout(3,2);
-title(graphmaker3000.N5512,'NACA 5512')
-for i = 11:16
-    nexttile
-    hold on
-    plot(results.Background_off.fspan,results.(fn_main{i}).P)
-    plot(results.Background_off.fspan,results.(fn_main{i}).filtered_P)
-    xlabel('Frequency [Hz]')
-    legend({'Power spectrum','Filtered power spectrum'})
-    hold off
-end
-
-figure()
-graphmaker3000.N63418 = tiledlayout(3,2);
-title(graphmaker3000.N63418,'NACA 63-418')
-for i = 17:22
-    nexttile
-    hold on
-    plot(results.Background_off.fspan,results.(fn_main{i}).P)
-    plot(results.Background_off.fspan,results.(fn_main{i}).filtered_P)
-    xlabel('Frequency [Hz]')
-    legend({'Power spectrum','Filtered power spectrum'})
-    hold off
-end
-
-figure()
-graphmaker3000.N64421 = tiledlayout(3,2);
-title(graphmaker3000.N64421,'NACA 64-421')
-for i = 23:28
-    nexttile
-    hold on
-    plot(results.Background_off.fspan,results.(fn_main{i}).P)
-    plot(results.Background_off.fspan,results.(fn_main{i}).filtered_P)
-    xlabel('Frequency [Hz]')
-    legend({'Power spectrum','Filtered power spectrum'})
-    hold off
-end
-
+if option.gm3k == 1
+    GraphMaker3000(results,corrected_audio,fn_main,option)
 end
 
 %% SPL
